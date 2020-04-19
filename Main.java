5da40a8c870f82a3f9eee36c9277de00e2cd5b31
nurzhanar <57892976+nurzhanar@users.noreply.github.com>
2020-04-19 23:51:14 +0600

    Week 2.Lec-3,4.Task 1

--- Main.java
+++ Main.java
@@ -1,17 +1,24 @@
 package com.company;
-interface ExcellentStudent{
-    public double excellentScholarship=30000;
+
+interface Employee{
+    public void getOverallIncome();
+    public void getJobStartDate();
 }
-interface BadStudent{
-    public double badScholarship=20000;
-}
-class GoodStudent implements ExcellentStudent,BadStudent{
-    public double scholarship=(excellentScholarship+badScholarship)/2;
-}
-public class Main {
-    public static void main(String[] args) {
-	GoodStudent Darina=new GoodStudent();
-	System.out.print(Darina.scholarship);
+class Student implements Employee{
+    public void getOverallIncome(){
+        System.out.println("Overall income 35000");
+
+    }
+
+    public void getJobStartDate(){
+        System.out.println("start date 15.02.2012");
     }
 }
+public class Main {
 
+    public static void main(String[] args) {
+	Student Darina=new Student();
+	Darina.getJobStartDate();
+	Darina.getOverallIncome();
+    }
+}
